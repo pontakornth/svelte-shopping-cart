8c1342f5ce7151a75e5cc98f461ec17e863beea7
pontakornth <pontakorn61@gmail.com>
2021-07-03 16:41:34 +0700

    feat: Add shop item component

---
+++ src/components/ShopItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type ShopItem from "../types/ShopItem"
+	export const item: ShopItem = {id: '', name: '', price: 0}
+</script>
+
+<div class="shop-item">
+	<p class="name">{item.name}</p>
+	<p class="price">{item.price}</p>
+	<butto class="add-button">Add to cart</butto>
+</div>
---
+++ src/types/ShopItem.ts
@@ -0,0 +1,7 @@
+type ShopItem = {
+	id: string;
+	name: string;
+	price: number;
+}
+
+export default ShopItem;
