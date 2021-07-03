4318c2db7462ff1a0807d1a470b49d0dbf0f9e46
pontakornth <pontakorn61@gmail.com>
2021-07-03 16:32:02 +0700

    feat: Do the basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,38 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let searchTerm = ""
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+<main class="main">
+  <input type="text" class="search-bar" bind:value={searchTerm} />
+  <div class="items-display">
+    <ul class="items-list">
+      <div class="item">
+        <p class="item-name">Paranormal Book</p>
+        <p class="price">120 Baht</p>
+        <button class="add-button">Add to cart</button>
+      </div>
+    </ul>
+  </div>
+  <div class="cart">
+    No items in the cart
+  </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  .main {
+    display: grid;
+    grid-template-areas:
+    "search search search" 
+    "content content sidebar" ;
+    grid-gap: 10px;
+  }
+  .search-bar {
+    grid-area: search;
+    padding: .85em;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+  .items-display {
+    grid-area: content;
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
