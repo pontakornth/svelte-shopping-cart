c2a1fee328beb03869b7e22c59ccb2afb2da88bd
pontakornth <pontakorn61@gmail.com>
2021-07-03 16:51:43 +0700

    refactor: Make them into component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+  import ShopItem from "./components/ShopItem.svelte"
+  import type Item from "./types/ShopItem"
+  let items: Item[] = [
+    {
+      id: "rsitbkrseitk",
+      name: "Todoroki Chocolate",
+      price: 20
+    },
+    {
+      id: "riwkdyw3kdy",
+      name: "Deku Breaker",
+      price: 340
+    }
+  ]
   let searchTerm = ""
 </script>
 
@@ -6,11 +20,9 @@
   <input type="text" class="search-bar" bind:value={searchTerm} />
   <div class="items-display">
     <ul class="items-list">
-      <div class="item">
-        <p class="item-name">Paranormal Book</p>
-        <p class="price">120 Baht</p>
-        <button class="add-button">Add to cart</button>
-      </div>
+      {#each items as item (item.id)}
+      <ShopItem {item} />
+      {/each}
     </ul>
   </div>
   <div class="cart">
@@ -33,6 +45,12 @@
 
   .items-display {
     grid-area: content;
+  }
+
+  .items-list {
+    display: grid;
+    grid-template-areas: repeat(3, 1fr);
+    grid-gap: 5px;
   }
 
 </style>
